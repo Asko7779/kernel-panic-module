2ebe7b3e3003c7290bf0baa853867ce2bb884e16
Asko7779 <169293034+Asko7779@users.noreply.github.com>
2025-04-04 00:25:47 +0200

    Update panic.cpp

--- panic.cpp
+++ panic.cpp
@@ -15,7 +15,7 @@ constexpr int SCREEN_H = 25;
 constexpr uint8_t BG = 0x0F;    // "white on black" text constant
 
 
-// string output system, cut off the integer part
+// string output system, i cut off the integer part
 
 class VGA {
 public:
